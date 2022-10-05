14d1105288975ade57fc62065c4d178d3ae15b53
Archit Jain <arcyjain2002@gmail.com>
2022-10-05 11:19:48 +0530

    modified the event schema

--- backend/server/models/event.js
+++ backend/server/models/event.js
@@ -1,25 +1,29 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const eventSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    startTime:{
-        type:String,
-        required:true
-    },
-    endTime:{
-        type:String,
-        required:true
-    },
-    participant:{
-        type:Number,
-        required:true,
-    },
-    venue:{
-        type:String,
-    }
+const eventSchema = new mongoose.Schema({
+  eventId: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  startTime: {
+    type: String,
+    required: true,
+  },
+  endTime: {
+    type: String,
+    required: true,
+  },
+  capcity: {
+    type: Number,
+    required: true,
+  },
+  venue: {
+    type: String,
+  },
 });
 
-module.exports=mongoose.model('Event',eventSchema);
+module.exports = mongoose.model("Event", eventSchema);
